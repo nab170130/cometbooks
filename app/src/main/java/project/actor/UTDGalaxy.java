7de398cf0e030a3c99dc3d9b8738e26687bb8ed3
nab170130 <nab170130@utdallas.edu>
2022-12-10 16:29:39 -0600

    Dummy Actors

---
+++ app/src/main/java/project/actor/UTDGalaxy.java
@@ -0,0 +1,22 @@
+package project.actor;
+
+import project.core.User;
+
+public class UTDGalaxy 
+{
+    public static User login(String netID, String password)
+    {
+        User user = null;
+
+        if(netID == "abc123456" && password == "pass1234")
+        {
+
+        }
+        else if(netID == "zyx654321" && password == "pass4321")
+        {
+            
+        }
+
+        return user;
+    }
+}
