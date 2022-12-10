278b0e7a5f63386671046d3329f5e40d5cdfd105
srithas <90726354+srithas@users.noreply.github.com>
2022-12-10 16:25:04 -0600

    Update Controller.java

--- app/src/main/java/project/core/Controller.java
+++ app/src/main/java/project/core/Controller.java
@@ -2,54 +2,57 @@ package project.core;
 
 public class Controller 
 {
-	public List<SalesListing> selectedBook(Textbook selectedBook)
+	public List<SalesListing> selectBook(Textbook selectedTextbook)
 	{
-		
+		return selectedTextbook.getMatchingListings();
 	}
 	
-	public HashMap<String, String> openSearchDialog()
-	{
+	// public HashMap<String, String> openSearchDialog()
+	// {
 		
-	}
+	// }
 	
 	public List<Textbook> performSearch(HashMap<String, String> searchParameters)
 	{
-		
+		return BookDBAdapter.getInstance().getMatchingBooks(searchParameters);
 	}
-	
+	// Needs to completed
 	public List<Conversation> navigateToConversationWindow()
 	{
-		
+		Conversation cn = new Transaction().getConversation();
 	}
 	
 	public List<Transaction> navigateToTransactionsWindow()
 	{
-		
+		return new User().getTransactions();
 	}
 	
 	public List<Textbook> navigateToBuyWindow()
 	{
-		
+		return new Buyer().getRecommendedTextbooks();
 	}
 	
 	public Conversation checkoutListing(SalesListing selectedListing)
 	{
-		
+		selectedListing.placeOnHold();
+		Transaction t = new Transaction(buyer, selectedListing); // need to add Buyer as member variable for controller class
+		return new Conversation(); // ?????
 	}
 	
 	public List<Textbook> addBookToWishlist(Textbook selectedBook)
 	{
-		
+		return new Buyer().addBookToWishlist(selectedBook); // which user is this method creating? no netid?
 	}
 	
 	public void completeTransaction(Transaction buyerSellerTransaction)
 	{
 		
+		// will write after we add buyer and seller as data members
 	}
 	
 	public Conversation sendMessage(Message enteredMessage)
 	{
-		
+		// will write after we add buyer and seller as data members
 	}
 	
 	public void selectConversation(Conversation conversation)
@@ -61,65 +64,4 @@ public class Controller
 	{
 		
 	}
-}
-    public List<SalesListing> selectedBook(Textbook selectedBook)
-	{
-		
-	}
-
-    public HashMap<String, String> openSearchDialog()
-	{
-		
-	}
-
-    public List<Textbook> performSearch(HashMap<String, String> searchParameters)
-	{
-		
-	}
-
-    public List<Conversation> navigateToConversationWindow()
-	{
-		
-	}
-
-    public List<Transaction> navigateToTransactionsWindow()
-	{
-		
-	}
-
-    public List<Textbook> navigateToBuyWindow()
-	{
-		
-	}
-
-    public Conversation checkoutListing(SalesListing selectedListing)
-	{
-		
-	}
-
-    public List<Textbook> addBookToWishlist(Textbook selectedBook)
-	{
-		
-	}
-
-    public void completeTransaction(Transaction buyerSellerTransaction)
-	{
-		
-	}
-
-    public Conversation sendMessage(Message enteredMessage)
-	{
-		
-	}
-
-    public void selectConversation(Conversation conversation)
-	{
-		
-	}
-
-    public void selectTransaction(Transaction buyerSellerTransaction)
-	{
-		
-	}
-
 }
