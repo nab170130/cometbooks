d4f0cf72c196b57e3848686c1561cdacd52991ef
Jeel Patel <JeelPatel@Jeels-MacPro.attlocal.net>
2022-12-10 14:51:56 -0600

    Updated Transaction.java

--- app/src/main/java/project/core/Transaction.java
+++ app/src/main/java/project/core/Transaction.java
@@ -9,38 +9,43 @@ public class Transaction
 	
 	
 	public Transaction(Buyer buyer, SalesListing selectedListing) 
-	{ // Both types are classes
-		
+	{ 
+		this.setTransactionID();
+		TransactionDBAdapter td = new TransactionDBAdapter();
+		td.storeTransaction();//Are we sending this object/Transaction object as an argument of this method???????????????
 	}
 
-//	public Transaction(TransactionRecord transactionRecord, SalesListing salesListing) 
-//	{//TransactionRecord IS A CLASS
-//		
-//	}
-	
-	
-//	public Conversation getConversation() 
-//	{//Conversation is a class
-//		
-//	}
-	
-	public void setTransactionID() 
-	{
-		
+	public Transaction(TransactionRecord transactionRecord, SalesListing salesListing) 
+	{//TransactionRecord IS A CLASS
+		this.buyer = new Buyer(transactionRecord);
 	}
+	
+	
+	public Conversation getConversation() 
+	{//Conversation is a class
+		MessageDBAdapter md = new MessageDBAdapter(); // Class to be cretaed
+		return md.getCoversationFromTransaction(this.transactionID);
+	}
+	
+	// public void setTransactionID() 
+	// {
+		//--------Need to discuus
+	// }
 	
 	public void markBuyerComplete() 
 	{
-		
+		TransactionDBAdapter td = new TransactionDBAdapter();
+		td.markBuyerComplete(this);
 	}
 	public void markSellerComplete() 
 	{
-		
+		TransactionDBAdapter td = new TransactionDBAdapter();
+		td.markSellerComplete(this);
 	}
 	
-	public long getTransactionID() 
+	public String getTransactionID() //Do we need chnage the data-ttype over here as we delcared ID as a string and In class-diagram we kept return data-type as long
 	{
-		return 0;
+		return this.transactionID;
 	}
 	
 }
