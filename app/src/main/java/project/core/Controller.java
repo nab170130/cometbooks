14d2a389b5cffeba1c5c2a2064339a3780472087
srithas <90726354+srithas@users.noreply.github.com>
2022-12-10 11:45:14 -0600

    Update Controller.java

--- app/src/main/java/project/core/Controller.java
+++ app/src/main/java/project/core/Controller.java
@@ -2,4 +2,63 @@ package project.core;
 
 public class Controller 
 {
+  public List<SalesListing> selectedBook(Textbook selectedBook)
+	{
+		
+	}
+
+    public HashMap<String, String> openSearchDialog()
+	{
+		
+	}
+
+    public List<Textbook> performSearch(HashMap<String, String> searchParameters)
+	{
+		
+	}
+
+    public List<Conversation> navigateToConversationWindow()
+	{
+		
+	}
+
+    public List<Transaction> navigateToTransactionsWindow()
+	{
+		
+	}
+
+    public List<Textbook> navigateToBuyWindow()
+	{
+		
+	}
+
+    public Conversation checkoutListing(SalesListing selectedListing)
+	{
+		
+	}
+
+    public List<Textbook> addBookToWishlist(Textbook selectedBook)
+	{
+		
+	}
+
+    public void completeTransaction(Transaction buyerSellerTransaction)
+	{
+		
+	}
+
+    public Conversation sendMessage(Message enteredMessage)
+	{
+		
+	}
+
+    public void selectConversation(Conversation conversation)
+	{
+		
+	}
+
+    public void selectTransaction(Transaction buyerSellerTransaction)
+	{
+		
+	}
 }
